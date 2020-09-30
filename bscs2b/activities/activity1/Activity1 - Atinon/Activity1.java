2f546fd94fb91bd09c9ec0a1e9f91e7cd6aa352d
Mark Vincent Atinon <mark_atinon@yahoo.com>
2020-09-30 14:03:37 +0800

    Activity1
    Activity 1 page 88 no.1-3

---
+++ bscs2b/activities/activity1/Activity1 - Atinon/Activity1.java
@@ -0,0 +1,17 @@
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+             
+        //variables
+        int num = 10;
+        char let = 'a';
+        boolean tru = true;
+        String lett = "Hello";
+        
+        System.out.println("The Value of integer is: " + num +
+        "\nThe Value of Character Data Type is: " + let + 
+        "\nThe Value of Boolean is: " +tru+
+        "\nThe Value of String is: " +lett ); 
+    }
+}
