aefdc9e2c4c939a404a3ac81c65fa0d8f4d3a421
Dave Zeus O. Nadura <59811221+zeusssieee@users.noreply.github.com>
2020-10-01 10:28:12 +0800

    Activity1 - Nadura
    Activity page 88 no.1-3

---
+++ bscs2b/activities/activity1/Activity 1 - Nadura/Activit1.java
@@ -0,0 +1,30 @@
+package Activity1;
+
+public class Activity1 {
+
+
+public static void main (String args){
+
+
+
+int num = 10;
+
+char let = 'a';
+
+boolean tru = true;
+
+string left = "Hello";
+
+
+System.out.println ("The value of integer is: "+ num + "
+
+"\nThe value of Character Data Type is:" + let +
+
+"\nThe value of Boolean is:" + tru +
+
+"\nThe value of string is: " + lett );
+
+
+   
+  }
+}
