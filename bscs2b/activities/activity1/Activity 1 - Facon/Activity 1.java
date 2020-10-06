1c6b2738867dd00f57c04ab9f71626242f52c4c1
Christian Facon <72431639+Christian08125@users.noreply.github.com>
2020-10-06 15:21:07 +0800

    Activity 1 - Facon

---
+++ bscs2b/activities/activity1/Activity 1 - Facon/Activity 1.java
@@ -0,0 +1,17 @@
+
+public class Activity1 {
+    
+    public static void main(String[] args) {
+        
+        int num = 10;
+        char let = 'a';
+        boolean res = true; 
+        String str = "Hello";
+        
+        System.out.println("Number  = "+(num));
+        System.out.println("letter  = "+(let));
+        System.out.println("result = "+(res));
+        System.out.println("str = " +(str));
+    }
+}
+
