b8782fd026d55a2a780f181157338df40a5ed095
Luieka224 <57333909+Luieka224@users.noreply.github.com>
2020-10-06 23:23:23 +0800

    Update TemperatureConverter.java
    Changed class name to TemperatureConverter from Main

--- bscs2b/activities/activity3/Activity 3 - Malimit/TemperatureConverter.java
+++ bscs2b/activities/activity3/Activity 3 - Malimit/TemperatureConverter.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
-
+public class TemperatureConverter {
   public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
       System.out.print("Enter temperature value in celsius: ");
