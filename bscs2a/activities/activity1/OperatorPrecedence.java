10ad0fb8e0a738ac13dbb1f5aeb632609c7df162
Aljhsrdnl <59908933+Aljhsrdnl@users.noreply.github.com>
2020-10-06 16:43:47 +0800

    Sardiniola Activity 1

---
+++ bscs2a/activities/activity1/OperatorPrecedence.java
@@ -0,0 +1,7 @@
+public class OperatorPrecedence {
+    public static void main (String[] args) {
+        System.out.println("(((a/b)^c)^((((d-e)+f)-(g*h))+i))");
+        System.out.println("(((((((3*10)*2)/15)-2)+4)^2)^2)");
+        System.out.println("((r^((((s*t)/u)-v)+w))^(x-(y++)))");
+    }
+}
