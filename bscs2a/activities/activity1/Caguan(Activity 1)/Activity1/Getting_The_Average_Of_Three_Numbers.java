146a42c497daba2b91965acc5a572da9a69a5a37
XyphrusCaguan <59828379+XyphrusCaguan@users.noreply.github.com>
2020-10-06 22:19:05 +0800

    Add files via upload

---
+++ bscs2a/activities/activity1/Caguan(Activity 1)/Activity1/Getting_The_Average_Of_Three_Numbers.java
@@ -0,0 +1,17 @@
+package javaapplication.Activity1;
+
+public class Getting_The_Average_Of_Three_Numbers {
+    public static void main(String[] args){
+        int num = 10;
+        int num2 = 20;
+        int num3 = 45;
+        int total = num + num2 + num3;
+        int average = total/3;
+        
+        System.out.println("Number 1 = " + num);
+        System.out.println("Number 2 = " + num2);
+        System.out.println("Number 3 = " + num3);
+        System.out.println("Average is = " + average);
+    }
+    
+}
