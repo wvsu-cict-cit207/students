8a7e6a7291184f900b20c42e17430ef496d2d9a1
Kenny Ackerman <59812125+AckermanKenny@users.noreply.github.com>
2020-10-07 18:03:57 +0800

    Activity 2 - Combalicer

---
+++ bscs2b/activities/activity2/Activity 2 - Combalicer/Activity2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Activity2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter number: ");
+        int num2 = sc.nextInt();
+        
+        System.out.println("Sum = " + (num1 + num2) +
+                            "\nDifference = " + (num1 - num2) +
+                            "\nProduct = " + (num1 * num2) + 
+                            "\nQuotient =  " + (num1 / num2) +
+                            "\nRemainder = " + (num1 % num2));
+    
+    
+}
+    
+    }
+// John Carlo O. Combalicer
