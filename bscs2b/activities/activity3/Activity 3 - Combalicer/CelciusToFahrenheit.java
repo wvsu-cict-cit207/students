c6e2df89910a760f9dfaa353a2240a7605220f46
Kenny Ackerman <59812125+AckermanKenny@users.noreply.github.com>
2020-10-07 19:35:49 +0800

    Activity 3 - Combalicer

---
+++ bscs2b/activities/activity3/Activity 3 - Combalicer/CelciusToFahrenheit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CelciusToFahrenheit {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Celcius: ");
+        
+        double celcius = sc.nextDouble();
+        double fahrenheit = (celcius * 9/5) + 32;
+        
+        System.out.println("Fahrenheit = " + fahrenheit);
+}
+    
+    }
+// John Carlo O. Combalicer
