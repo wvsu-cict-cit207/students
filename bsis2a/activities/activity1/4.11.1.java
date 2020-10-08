a25c75e2ae885097b92b68d6a3ece4a81623d98d
Izelmae <72552677+Izelmae@users.noreply.github.com>
2020-10-08 19:07:55 +0800

    Gapi-activity1

---
+++ bsis2a/activities/activity1/4.11.1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
+
+        System.out.print(" \nnumber ="+ number);
+        System.out.print("\nletter =" + letter);
+        System.out.print("\nresult =" + result);
+        System.out.print("\nstr =" +str);
+
+
+	// write your code here
+    }
+}
