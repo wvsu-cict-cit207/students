fc6206a8d13f846e9de25d1d1e480ee89a507b3d
jfrms <59947475+jfrms@users.noreply.github.com>
2020-10-12 14:18:48 +0800

    Add files via upload

---
+++ bscs2a/activities/activity1/FORMENTO-ACTIVITY1/Activity1-4.11.1/src/activity1/pkg4/pkg11/pkg1/Activity14111.java
@@ -0,0 +1,21 @@
+
+ */
+package activity1.pkg4.pkg11.pkg1;
+
+
+public class Activity14111 {
+
+
+    public static void main(String[] args) {
+       int Number = 10;
+       char Letter = 'a';
+       boolean result = true;
+       String str = "hello";
+       
+       System.out.println("Number = " + Number);
+       System.out.println("letter = " + Letter);
+       System.out.println("result = " + result);
+       System.out.println("str = " + str);
+    }
+    
+}
