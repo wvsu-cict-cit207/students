652f60b0056385a9d13a52e4bd0b5df443658f3f
Jed Adrian Denosta <59909864+JedAdrianDenosta@users.noreply.github.com>
2020-10-13 16:29:03 +0800

    Create Main.java

---
+++ bscs2b/activities/activity1/Activity 1 - Denosta/Exercise 1 -Denosta/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int number = 10;
+	    char letter = 'a';
+	    boolean result = true;
+	    String str = "hello";
+
+        System.out.println("Number = " + number);
+        System.out.println("letter = " + letter);
+        System.out.println("result = " + result);
+        System.out.println("str = " + str);
+    }
+}
